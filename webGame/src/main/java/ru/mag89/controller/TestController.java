1cef2fc00fd72d6c37fa3df7552a81c60fe823d2
mag89 <tyr-89@mail.ru>
2020-10-23 09:37:58 +0300

    thymeleaf on

--- webGame/src/main/java/ru/mag89/controller/TestController.java
+++ webGame/src/main/java/ru/mag89/controller/TestController.java
@@ -1,16 +1,15 @@
 package ru.mag89.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/")
+@Controller
 public class TestController {
 
     @GetMapping("/test")
-    public String test() {
-
+    public String test(Model model) {
+        model.addAttribute("message", "That was tested from thymeleaf");
         return "tested";
     }
 }
