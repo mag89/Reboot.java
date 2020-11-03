470fe858ae845dbcf43d0e966b0cf1f8cd36b424
mag89 <tyr-89@mail.ru>
2020-11-03 16:26:31 +0300

    simple select query and getting the results

--- jdbcLesson/src/main/java/Main.java
+++ jdbcLesson/src/main/java/Main.java
@@ -1,7 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,16 +6,33 @@ public class Main {
         try (Connection conn = DriverManager.getConnection("jdbc:sqlite:test.db");
              Statement statement = conn.createStatement()
         ) {
-//            conn.setAutoCommit(false);
-            statement.execute("CREATE TABLE IF NOT EXISTS contacts " +
-                    "(name TEXT, phone INTEGER, email TEXT)");
+//            conn.setAutoCommit(false); // отключаем авокомит по умолчанию
+//
+//            statement.execute("CREATE TABLE IF NOT EXISTS contacts " +
+//                    "(name TEXT, phone INTEGER, email TEXT)");
+//
+//            statement.execute("DELETE FROM contacts");
+//
+//            statement.execute("INSERT INTO contacts (name, phone, email)" +
+//                    "VALUES ('artur', 9191041052, 'tyr-89@mail.ru')");
+//
+//            statement.execute("INSERT INTO contacts (name, phone, email)" +
+//                    "VALUES ('Julia', 9153333405, 'julkaTsap@yandex.ru')");
+//
+//            statement.execute("UPDATE contacts SET email='superArturEmail.com' " +
+//                                 "WHERE name='artur'");
+//
+//            conn.commit();
 
-            statement.execute("DELETE FROM contacts WHERE name='artur'");
+            statement.execute("select * from contacts");
+            ResultSet result = statement.getResultSet();
 
-            statement.execute("INSERT INTO contacts (name, phone, email) " +
-                    "VALUES ('artur', 9191041052, 'tyr-89@mail.ru')");
-
-            statement.execute("UPDATE contacts SET email='superEmail.com' WHERE name='artur'");
+            while (result.next()) {
+                System.out.printf("%s %s %s", result.getString("name"), result.getInt("phone"),
+                        result.getString("email"));
+                System.out.println();
+            }
+            result.close(); // resultSet это ресурс, после использования нужно закрыть
 
         } catch (SQLException e) {
             System.out.println("something went wrong with DB connection: " + e.getMessage());
