31b909cb524addd8815289cd9f24f2a908beeaef
mag89 <tyr-89@mail.ru>
2021-04-10 11:07:04 +0300

    Java lambda expression can references outside the expression through variables and if it reference is local variable, the local variable must be final/effective final, and cant be changed after lambda implementation
     Is same as true for static and regular fields, but it allowed change value after lambda implementation

--- lambda/src/main/java/org/example/lambda/LambdaExemples2.java
+++ lambda/src/main/java/org/example/lambda/LambdaExemples2.java
@@ -4,14 +4,29 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class LambdaExemples2 {
+    static String thirdText = "Hey, a third text!";
+
+    private String forthText = "Forth Text";
+
+    private void doIt() {
+        MyInterface myInterface3 = (String text) -> {
+            System.out.println(text + " " + forthText);
+        };
+
+        myInterface3.printIt("Ather");
+
+        forthText = "Bebebebebe!";
+
+        myInterface3.printIt("Ather");
+    }
 
     public static void main(String[] args) throws IOException {
 
-/**
- * Java lambda expression can implement interfaces with
- * a single unimplemented method (abstract) method,
- * but as many default or static methods as yuo like.
- * **/
+        /**
+         * Java lambda expression can implement interfaces with
+         * a single unimplemented method (abstract) method,
+         * but as many default or static methods as yuo like.
+         * **/
 
 
         // in MyInterface 3 methods, but only one method is abstract
@@ -28,6 +43,33 @@ public class LambdaExemples2 {
 
 
         MyInterface.printItToSystemOut("Hello from static context");
+
+
+        /**
+         *Java lambda expression can references outside the expression through variables
+         * and if it reference is local variable, the local variable must be final/effective final,
+         * and cant be changed after lambda implementation
+         *
+         * Is same as true for static and regular fields, but it allowed change value after lambda implementation
+         */
+
+        final String otherText = "Hello: "; //the local variable does not changed value after has been set in first time,
+        //because it used in lambda implementation, and must be final/effective final
+
+        MyInterface myInterface3 = (String text) -> {
+            System.out.println(otherText + text + " " + thirdText);
+        };
+
+        myInterface3.printIt("WORLD");
+
+        //note this, a static field can be changed after lambda implementation
+        thirdText = "Blabalblabla!";
+
+        myInterface3.printIt("WORLD");
+
+        //and this, with regular field
+        LambdaExemples2 instance = new LambdaExemples2();
+        instance.doIt();
 
 
     }
