d329a7712e61e7ed8383b8578c5952a8a8a738b3
mag89 <tyr-89@mail.ru>
2021-07-11 14:27:09 +0400

    added 34 solution

--- sql-ex/src/main/java/34.sql
+++ sql-ex/src/main/java/34.sql
@@ -1,3 +1,3 @@
-// ships.launched >= 1922 && classes.displacement > 35_000 && classes.type='bb'
+-- ships.launched >= 1922 && classes.displacement > 35_000 && classes.type='bb'
 
 select ships.name from classes join ships on classes.class=ships.class where ships.launched >= 1922 and classes.displacement > 35000 and classes.type='bb'
